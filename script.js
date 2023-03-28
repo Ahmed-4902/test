3e39edbbe09ff2418485e5e949f37b44f96f27a6
Ahmed <teleb0.1am@gmail.com>
2023-03-28 19:08:06 +0200

    nothing

---
+++ script.js
@@ -0,0 +1,21 @@
+var form = document.getElementById("mail-form");
+
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	var xhr = new XMLHttpRequest();
+	var url = "send-mail.php";
+	var formData = new FormData(form);
+
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			console.log(xhr.responseText);
+			// Show success message to user
+		}
+	};
+
+	xhr.send(formData);
+});
